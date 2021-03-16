de3ee28a87dcee73f6af3576a22926d2677b8587
Ahmad Safar <ahmadsafar88@gmail.com>
2021-03-16 21:49:01 +0800

    Fix skalian reformatting

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 
-export const Search =() =>{
-    return(
-        <div class="bg-white col-span-1 md:col-span-2 shadow p-4 h-16 flex">
-            <input class="h-8 w-screen border shadow-sm rounded p-2 transition-all duration-700 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" type="text" placeholder="Github Username" />
-            <button class="bg-blue-400 h-8 flex mx-2 hover:bg-blue-300 transition-all duration-700 rounded text-white p-2 pl-4 pr-4 place-content-center">
-            <svg src="assets/images/search.svg" width="10px" class="mr-2 fill-current text-white" ></svg><p class="font-semibold text-xs">Search</p>
+import SearchIcon from '../search.svg'
+
+export const Search = () => {
+    return (
+        <div className="bg-white col-span-1 md:col-span-2 shadow p-4 h-16 flex">
+            <input className="h-8 w-screen border shadow-sm rounded p-2 transition-all duration-700 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent" type="text" placeholder="Github Username" />
+            <button className="bg-blue-400 h-8 flex mx-2 hover:bg-blue-300 transition-all duration-700 rounded text-white p-2 pl-4 pr-4 place-content-center">
+                <img alt="" src={SearchIcon} className="mr-2 fill-current text-white" />
+                <p className="font-semibold text-xs">Search</p>
             </button>
         </div>
     )
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,32 +1,30 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-
 /* eslint-disable array-callback-return */
-export const Users =(props) =>{
-    const {datas} = props
-    datas.map((value,index)=>{
-        console.log(index,value.login)
+export const Users = ({ data }) => {
+    data.map((value, index) => {
+        console.log(index, value.login)
     })
 
-        return (
-            <div>
-                {datas.map(data => {
-                       <div class="px-2 flex items-center justify-between my-4">
-                        <div class="w-16"><br />
-                        <img class="w-12 h-12 border-white border-2 shadow rounded-full" src="{data.avatar_url}"  alt=""/>
+    return (
+        <div className="test">
+            {data.map(data => (
+                <div className="px-2 flex items-center justify-between my-4">
+                    <div className="w-16"><br />
+                        <img className="w-12 h-12 border-white border-2 shadow rounded-full" src={data.avatar_url} alt="" />
+                    </div>
+                    <div className="flex-1 pl-2">
+                        <div className="text-gray-800 font-semibold">
+                            {data.login}
                         </div>
-                        <div class="flex-1 pl-2">
-                            <div class="text-gray-800 font-semibold">
-                                {data.login}
-                            </div>
-                            <div class="text-gray-700 font-thin">
-                                {data.html_url}
-                            </div>
+                        <div className="text-gray-700 font-thin">
+                            {data.html_url}
                         </div>
-                        <div class="text-gray-600">20 Posts</div>
-                        </div>
-                    })}
-            </div>
-        )
+                    </div>
+                    <div className="text-gray-600">20 Posts</div>
+                </div>
+            ))}
+        </div>
+    )
 
-    
+
 }
